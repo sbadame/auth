a3441fd3533f2cfb97d63d51e20be32ad22ccbfe
Sandro Badame <sandro@sandros-mbp.lan>
2022-04-25 15:54:27 -0400

    Add a test server to act as a backend

---
+++ test/test_server.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"html"
+	"log"
+	"net/http"
+
+	"os"
+)
+
+func main() {
+	// https://cloud.google.com/run/docs/reference/container-contract#port
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8091"
+	}
+	log.Printf("Serving test server on %s.\n", port)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+	})
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
