9dc59ef773f7471f1967a29f9c75af1473f939a6
Sandro Badame <sbadame@gmail.com>
2022-10-27 22:14:20 -0400

    Add more info about validating the webauthn challenge.

--- internal/auth/webauthn.go
+++ internal/auth/webauthn.go
@@ -11,6 +11,7 @@ func validateResponse(w http.ResponseWriter, r *http.Request) {
 	// TODO: Verifying that the challenge is the same as the challenge that was sent
 	// TODO: Ensuring that the origin was the origin expected
 	// TODO: Validating that the signature over the clientDataHash and the attestation using the certificate chain for that specific model of the authenticator
+	// https://goo.gl/yabPex
 	w.Header().Set("Content-Type", "application/json")
 	fmt.Fprintln(w, "{}")
 }
